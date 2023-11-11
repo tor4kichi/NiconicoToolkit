5e3a19f51aaad2884215e8a8550910795d91bb3d
tor4kichi <tor4kichi@gmail.com>
2023-11-11 09:27:37 +0900

    update to v0.5.2

--- NuSpec/build_nuget.cmd
+++ NuSpec/build_nuget.cmd
@@ -1,2 +1,1 @@
-nuget pack Package.nuspec -outputDirectory "C:\Users\tor4k\local_nuget" -p version=0.5.0
-pause
+nuget pack Package.nuspec -outputDirectory "C:\Users\tor4k\local_nuget" -p version=0.5.3
