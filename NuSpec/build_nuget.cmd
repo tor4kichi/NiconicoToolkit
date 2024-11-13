6bb4377ecc73980142914a4051b378889746a9a7
tor4kichi <tor4kichi@gmail.com>
2024-11-13 17:06:37 +0900

    .Net 9 対応

--- NuSpec/build_nuget.cmd
+++ NuSpec/build_nuget.cmd
@@ -1,1 +1,2 @@
-nuget pack Package.nuspec -outputDirectory "C:\Users\tor4k\local_nuget" -p version=0.7.0
+nuget pack Package.nuspec -outputDirectory "C:\Users\tor4k\local_nuget" -p version=0.8.0
+pause
